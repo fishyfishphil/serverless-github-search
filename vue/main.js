c197a6d9118b2d145e054fb019c3e8e8ebe34428
phil <phillipdebruin1@gmail.com>
2019-02-07 21:26:38 +0200

    Update front end

--- vue/main.js
+++ vue/main.js
@@ -1,23 +1,25 @@
 new Vue({
     el: '#root',
     data: {
+        items: [],
+        isFullPage: true,
         username: 'fishyfishphil'
     },
     methods: {
         find() {
+            const loadingComponent = this.$loading.open({
+                container: this.isFullPage ? null : this.$refs.element.$el
+            })
             axios.post('https://soadagyfl9.execute-api.eu-west-1.amazonaws.com/dev/find',
                 { user: this.username })
                 .then(response => {
-                    console.log(response);
+                    this.items = response.data
+                    loadingComponent.close()
+                    console.log(items)
                 })
                 .catch(error => {
-                    console.log(error);
+                    console.log(error)
                 })
         }
-    },
-    mounted() {
-        username = 'wesbos'
-        find()
-
     }
 })
